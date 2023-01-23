37c4961457152c54f5960f88c3766be8a2c0af5a
rajpravin010 <rajpravin010@gmail.com>
2023-01-23 12:52:41 +0530

    my project

---
+++ src/test/java/org/test/login/Company.java
@@ -0,0 +1,19 @@
+package org.test.login;
+
+public class Company {
+	private void tc1() {
+		System.out.println("Infosys");
+
+	}
+	private void tc2() {
+		System.out.println("TCS");
+
+		
+	}
+	private void tc3() {
+		System.out.println("wiprow");
+
+	}
+	
+
+}
---
+++ src/test/java/org/test/login/Sample.java
@@ -0,0 +1,19 @@
+package org.test.login;
+
+public class Sample {
+	private void tc1() {
+		System.out.println("praveen");
+
+	}
+	private void tc2() {
+		System.out.println("raja");
+
+		
+	}
+	private void tc3() {
+		System.out.println("kumar");
+
+	}
+	
+
+}
